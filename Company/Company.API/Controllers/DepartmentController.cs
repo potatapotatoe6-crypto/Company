872d67892df4e4cf42814ec690bb2a46c779fa40
potatapotatoe6-crypto <potatapotatoe6@gmail.com>
2026-07-31 15:14:04 +0530

    temp2: AddDepartmentController

--- Company/Company.API/Controllers/DepartmentController.cs
+++ Company/Company.API/Controllers/DepartmentController.cs
@@ -1,7 +1,20 @@
 ﻿namespace Company.API.Controllers
 {
-    public class DepartmentController : ControllerBase
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentController(IMediator mediator, IMapper mapper) : ControllerBase
     {
 
+        [HttpPost("AddDepartment")]
+        public async Task<IActionResult> AddDepartment(DepartmentModel model)
+        {
+            RegisterUserCommand command = mapper.Map<RegisterUserCommand>(model);
+            bool result = await mediator.Send(command);
+            if (!result)
+            {
+                return BadRequest("Username Already exists");
+            }
+            return Ok("User register successfully");
+        }
     }
 }
